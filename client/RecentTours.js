690fddc7687d01dbe56d9c176bda49db324734e8
Sashko Stubailo <sashko@stubailo.com>
2016-08-27 19:04:11 -0700

    Add targetRepository and description

--- client/RecentTours.js
+++ client/RecentTours.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import TourBadge from './TourBadge';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class RecentTours extends React.Component {
+class RecentTours extends React.Component {
   render() {
     return (
       <div>
         <h3>Recently added tours</h3>
-        <TourBadge />
-        <TourBadge />
+        { this.props.tours.map(tour => <TourBadge tour={tour} key={tour.repository} /> ) }
       </div>
     )
   }
 }
+
+export default createContainer(() => {
+  return {
+    tours: Tours.find().fetch(),
+  };
+}, RecentTours);
--- client/TourBadge.js
+++ client/TourBadge.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 export default class TourBadge extends React.Component {
   render() {
+    const { tour } = this.props;
+    const targetRepo = tour.targetRepository;
+    const targetUsername = targetRepo.split('/')[0];
+
     return (
       <div className="tour-badge">
-        <img src="https://github.com/facebook.png"/>
-        <h4 className="target-repo">facebook/react</h4>
-        <p className="tour-title">{"A walkthrough of React's reconciliation algorithm."}</p>
-        <p className="tour-credits">Contribute at <a href="#">dan_abramov/facebook-tour</a></p>
+        <img src={`https://github.com/${targetUsername}.png`}/>
+        <h4 className="target-repo">{targetRepo}</h4>
+        <p className="tour-title">{tour.description}</p>
+        <p className="tour-credits">
+          Contribute at <a href={`https://github.com/${tour.repository}`}>{tour.repository}</a>
+        </p>
       </div>
     )
   }
