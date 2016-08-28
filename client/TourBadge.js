c78972883654d86689775bb60423f283ca771a16
Sashko Stubailo <sashko@stubailo.com>
2016-08-27 19:10:49 -0700

    Add author

--- client/TourBadge.js
+++ client/TourBadge.js
@@ -4,12 +4,13 @@ export default class TourBadge extends React.Component {
   render() {
     const { tour } = this.props;
     const targetRepo = tour.targetRepository;
+    const authorUsername = tour.repository.split('/')[0];
     const targetUsername = targetRepo.split('/')[0];
 
     return (
       <div className="tour-badge">
         <img src={`https://github.com/${targetUsername}.png`}/>
-        <h4 className="target-repo">{targetRepo}</h4>
+        <h4 className="target-repo">{targetRepo} <span className="text-muted">by {authorUsername}</span></h4>
         <p className="tour-title">{tour.description}</p>
         <p className="tour-credits">
           Contribute at <a href={`https://github.com/${tour.repository}`}>{tour.repository}</a>
