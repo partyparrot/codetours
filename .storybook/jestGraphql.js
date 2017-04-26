1242e78cf5e1c7352947d5a6b244ef14fd71de23
xavcz <xavier.cazalot@gmail.com>
2017-04-26 08:35:41 +0200

    jest-graphql: use graphql-tag/loader directly by providing a context stub to the loader

--- .storybook/jestGraphql.js
+++ .storybook/jestGraphql.js
@@ -1,40 +1,9 @@
-// note:
-// `jest-transform-graphql` is obsolete at the moment
-// once we figure out how to use graphql-tag/loader correctly with jest
-// we'll open a PR on the transform package :)
-'use strict';
-
-const gql = require('graphql-tag');
-
-// eslint-disable-next-line
-function expandImports(source, doc) {
-  const lines = source.split('\n');
-  let outputCode = '';
-
-  lines.some(line => {
-    if (line[0] === '#' && line.slice(1).split(' ')[0] === 'import') {
-      const importFile = line.slice(1).split(' ')[1];
-      const parseDocument = `require(${importFile})`;
-      const appendDef = `doc.definitions = doc.definitions.concat(${parseDocument}.definitions);`;
-      outputCode += appendDef + '\n';
-    }
-    return line.length !== 0 && line[0] !== '#';
-  });
-
-  return outputCode;
-}
+const loader = require('graphql-tag/loader');
 
 module.exports = {
-  process(source) {
-    // line removed here: cacheable from webpack
-    // https://webpack.github.io/docs/loaders.html#cacheable
-    const doc = gql`${source}`;
-    const outputCode = `var doc = ${JSON.stringify(doc)};`;
-    const importOutputCode = expandImports(source, doc);
-
-    // uncomment for debug
-    // console.log(outputCode + '\n' + importOutputCode + '\n' + `module.exports = doc;`);
-
-    return outputCode + '\n' + importOutputCode + '\n' + `module.exports = doc;`;
+  process(src) {
+    // call directly the webpack loader with a mocked context
+    // as graphql-tag/loader leverages `this.cacheable()`
+    return loader.call({ cacheable() {} }, src);
   },
 };
